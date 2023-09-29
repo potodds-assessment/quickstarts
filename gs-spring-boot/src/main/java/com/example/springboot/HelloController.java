51424c5c89d5f119d3b045a473645f6d1c3d1d53
lapwahlee <lwlee2014@gmail.com>
2023-09-29 13:11:32 -0400

    forgot to save changes

--- gs-spring-boot/src/main/java/com/example/springboot/HelloController.java
+++ gs-spring-boot/src/main/java/com/example/springboot/HelloController.java
@@ -1,13 +1,9 @@
 package com.example.springboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 public class HelloController {
-
-    @GetMapping("/")
+    @GetMapping("/");
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from Spring Boot";
     }
 }
