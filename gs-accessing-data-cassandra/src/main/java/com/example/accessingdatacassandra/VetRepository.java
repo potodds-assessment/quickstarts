00f45185207fbd83c70c05a2ab1d731d6b2200e8
Lap Wah Lee <lapwah.lee@gmail.com>
2023-10-02 23:04:16 -0400

    Accessing Data with Cassandra https://spring.io/guides/gs/accessing-data-cassandra/

---
+++ gs-accessing-data-cassandra/src/main/java/com/example/accessingdatacassandra/VetRepository.java
@@ -0,0 +1,8 @@
+package com.example.accessingdatacassandra;
+
+import java.util.UUID;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VetRepository extends CrudRepository<Vet, UUID> {
+    Vet findByFirstName(String username);
+}
---
+++ gs-accessing-data-cassandra/src/test/java/com/example/accessingdatacassandra/AccessingDataCassandraApplicationTestsTest.java
@@ -0,0 +1,13 @@
+package com.example.accessingdatacassandra;
+
+import java.beans.Transient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AccessingDataCassandraApplicationTestsTest {
+    @Test
+    void contextLoads() {}
+}
+
