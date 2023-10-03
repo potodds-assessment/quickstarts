1073204b0166d7700e493154c6db1a81ab65a04d
lapwahlee <lwlee2014@gmail.com>
2023-10-03 12:52:05 -0400

    Building a RESTful Web Service with Spring Boot Actuator https://spring.io/guides/gs/actuator-service/

---
+++ gs-actuator-service/src/main/java/com/example/actuatorservice/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.example.actuatorservice;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+    
+}
